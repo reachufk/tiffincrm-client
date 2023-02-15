1af9b607dea4280be05656af371f134ab09df54c
Asif Dar <asif.dar@lelafe.com>
2023-02-15 21:34:49 +0530

    angular initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+import { AppComponent } from './app.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { PublicLayoutComponent } from './layouts/public-layout/public-layout.component';
+import { UserLayoutComponent } from './layouts/user-layout/user-layout.component';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule,
+    AppRoutingModule,
+    
+  ],
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent,
+    NavbarComponent,
+    FooterComponent,
+    SidebarComponent,
+    PublicLayoutComponent,
+    UserLayoutComponent
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { PublicLayoutComponent } from './layouts/public-layout/public-layout.component';
+import { UserLayoutComponent } from './layouts/user-layout/user-layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'public',
+    pathMatch: 'full',
+  },
+  {
+    path: 'public',
+    component: PublicLayoutComponent,
+    children: [{
+      path: '',
+      loadChildren: () => import('./public/public.module').then(m => m.PublicModule)
+    }]
+  },
+  {
+    path: 'user',
+    component: UserLayoutComponent,
+    children: [{
+      path: '',
+      loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+    }]
+  },
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+    children: [{
+      path: '',
+      loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+    }]
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes, {
+      useHash: true
+    })
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
