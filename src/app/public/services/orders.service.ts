56b23e9f8489fad5e32a2d66bc20bf69fe4ea5e8
Asif Dar <asif.dar@lelafe.com>
2023-03-02 20:42:02 +0530

    unwanted 72

--- src/app/public/services/orders.service.ts
+++ src/app/public/services/orders.service.ts
@@ -1,13 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { Observable } from 'rxjs';
-import  {io} from 'socket.io-client';
 import { FetchOrderModel } from 'src/app/shared/interfaces/fetch-completed-orders';
 @Injectable()
 export class OrdersService {
 
-  private socket: any;
   constructor(private http: HttpClient) {
   }
 
@@ -29,22 +26,9 @@ export class OrdersService {
   PlaceOrder(OrderModel:any){
     return this.http.post(`${environment.server}Orders/PlaceOrder`,OrderModel)
   }
-  FetchNewCreatedOrder(): Observable<any> {
-    return new Observable((observer) => {  this.socket.on('newOrder', (order) => {
-        observer.next(order);
-      });
-    });
-  }
 
   GetUserOrders(user:String){
     return this.http.get(`${environment.server}Orders/GetUserOrders/${user}`)
   }
-
-  disconnect(){
-    this.socket.on('disconnect', () => {
-      console.log('connected to server');
-    });
-  }
-
 
 }
