946dbf3fd0488bbadba782a5264a0e1ed1c973d2
shyyashowkat <shyxum96@gmail.com>
2023-03-04 17:24:58 +0530

    _

--- src/app/tiffin-landing/tiffin-landing.component.ts
+++ src/app/tiffin-landing/tiffin-landing.component.ts
@@ -1,4 +1,4 @@
-import { Component ,OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { AuthService } from '../auth/services/auth.service';
@@ -7,34 +7,63 @@ import * as regionData from './availRegions.json'
   selector: 'app-tiffin-landing',
   templateUrl: './tiffin-landing.component.html',
   styleUrls: ['./tiffin-landing.component.scss'],
-  providers:[MessageService]
+  providers: [MessageService]
 })
 export class TiffinLandingComponent implements OnInit {
-  
-  availablePlaces:Array<any>=[];
-  results:Array<any>=[];
-  selected :any
-  constructor(private authService:AuthService,private router:Router){
+  highLights: string[] = [];
+  availablePlaces: Array<any> = [];
+  results: Array<any> = [];
+  selected: any;
+  highLight: string = 'Hungry?';
+  counter: number = 0;
+  changeClass: boolean = true;
+  showSelected: boolean = false;
+  constructor(private authService: AuthService, private router: Router) {
 
   }
   ngOnInit(): void {
-    const {regions} = regionData
-    this.availablePlaces = regions
+    const { regions } = regionData;
+    this.availablePlaces = regions;
+    this.highLights = [
+      'Cooking gone wrong?',
+      'Movie marathon?',
+      'Unexpected guests?',
+      'Late night at office?',
+      'Hungry?'
+    ]
   }
 
-  search(event){
-    this.results = this.availablePlaces.filter((region:any)=> region?.label?.toLowerCase()?.includes(event?.query?.toLowerCase()))
+  search(event) {
+    this.results = this.availablePlaces.filter((region: any) => region?.label?.toLowerCase()?.includes(event?.query?.toLowerCase()))
   }
 
 
-  selectChange(region){
+  selectChange(region) {
+    this.showSelected = true;
     this.selected = region
   }
 
-  SetGo(){
+  SetGo() {
+    this.showSelected = false;
     this.authService.Region.next(this.selected);
-    localStorage.setItem('selectedRegion',JSON.stringify(this.selected));
+    localStorage.setItem('selectedRegion', JSON.stringify(this.selected));
     this.router.navigate(['/public/home'])
   }
 
+  skip() {
+    this.showSelected = false;
+    localStorage.removeItem('selectedRegion');
+    this.router.navigate(['/public/home'])
+  }
+
+  ngAfterViewInit() {
+    setInterval(() => {
+      if (this.counter == this.highLights.length) {
+        this.counter = 0;
+      }
+      this.highLight = this.highLights[this.counter];
+      this.changeClass = !this.changeClass;
+      this.counter++;
+    }, 3000)
+  }
 }
