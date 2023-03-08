f1eca0cbdac38af5d19895abdf75299ba28756b0
Asif Dar <asif.dar@lelafe.com>
2023-03-08 13:09:12 +0530

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  server:'http://localhost:3000/',
-  socketURL:'http://localhost:3000/'
+  server:'https://tiffin.ufksolutions.com/',
+  socketURL:'https://tiffin.ufksolutions.com/'
 };
